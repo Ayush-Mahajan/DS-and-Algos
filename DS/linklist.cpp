a78af22443636ad6f0d2a26049e0485c5820e212
Ayush Mahajan <ayushm0108@gmail.com>
2019-06-19 01:33:32 +0530

    updated linklist

--- DS/linklist.cpp
+++ DS/linklist.cpp
@@ -37,6 +37,37 @@ void insertAtHead(node*&head, int data){
     head = n;
 }
 
+void insertAtTail(node*&head, int data){
+    if(head==NULL){
+        head = new node(data);
+        return;
+    }
+    node*tail = head;
+    while(tail->next!=NULL){
+        tail = tail->next;
+    }
+    tail->next = new node(data);
+    return;
+}
+
+void insertAtMiddle(node*&head, int data, int pos){
+    if(head==NULL||pos==0){
+        insertAtHead(head, data);
+    } else if(pos>lenght(head)){
+        insertAtTail(head, data);
+    } else {
+        int jump=0;
+        node*temp=head;
+        while(jump<=pos-1){
+            temp = temp->next;
+            jump++;
+        }
+
+        node*n = new node(data);
+        n->next = temp->next;
+        temp->next = n;
+    }
+}
 // void insertAtTail(node*&head,int data){
 //     node*prv = head;
      
