c9087c461d6849ce78c879f91f77c8453f059230
Ayush-Mahajan <mail4ayushmahajan@gmail.com>
2019-07-25 23:54:04 +0530

    update

--- Algos/DP/Fib_fast.cpp
+++ Algos/DP/Fib_fast.cpp
@@ -4,9 +4,17 @@
 using namespace::std;
 
 #define long long long
-const long M = 1000000007
+const long M = 1000000007;
 map<long, long> F;
 
+long f(long n){
+    if(F.count(n)) return F[n];
+
+    long k = n/2;
+    if(n%2==0) {
+        return F[n] = (f(k)*f(k)+ f(k-1)*f(k-1))%M;
+    }
+}
 
 int main(){
 
